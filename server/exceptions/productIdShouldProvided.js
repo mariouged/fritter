fcc877f9b17694f4ac1ff688de38df8b89f50e83
Jaime <jaime.mansilla@between.tech>
2021-11-16 11:08:11 +0100

    Rename and structure folders

--- server/exceptions/productIdShouldProvided.js
+++
@@ -1,7 +0,0 @@
-export class ProductIdShouldProvided extends Error {
-  constructor (message) {
-    super(message);
-    this.name = 'ProductIdShouldProvided';
-    this.httpStatusCode = 400;
-  }
-}
